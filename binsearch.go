eebd22b7d4ab9f6ffdd478dc666ca3790c339095
AlasdairF <AlasdairF@users.noreply.github.com>
2014-08-06 01:01:10 +0700

    test

--- binsearch.go
+++ binsearch.go
@@ -101,7 +101,7 @@ func (f *Key_uint32) Find(thekey uint32) (uint64, bool) {
 		if (k<0 || k>1) {
 			return min, false
 		}
-		at := uint64(min+k*(max-min))
+		at := uint64(float32(min)+k*float32(max-min))
 	
 		current := f.Key[at]
 		if thekey<current {
