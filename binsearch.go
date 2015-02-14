730507832be9e66b564c1511befb566c3d56b449
AlasdairF <AlasdairF@users.noreply.github.com>
2015-02-15 00:59:06 +0700

    debug

--- binsearch.go
+++ binsearch.go
@@ -2166,35 +2166,35 @@ func (t *KeyBytes) Next() ([]byte, bool) {
 		case 0:
 			v := t.limit8[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit8[t.on8]))
-			return reverse8(v), int(v), eof
+			return reverse8(v), eof
 		case 1:
 			v := t.limit16[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit16[t.on8]))
-			return reverse16(v), int(v[1]), eof
+			return reverse16(v), eof
 		case 2:
 			v := t.limit24[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit24[t.on8]))
-			return reverse24(v), int(v[2]), eof
+			return reverse24(v), eof
 		case 3:
 			v := t.limit32[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit32[t.on8]))
-			return reverse32(v), int(v[3]), eof
+			return reverse32(v), eof
 		case 4:
 			v := t.limit40[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit40[t.on8]))
-			return reverse40(v), int(v[4]), eof
+			return reverse40(v), eof
 		case 5:
 			v := t.limit48[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit48[t.on8]))
-			return reverse48(v), int(v[5]), eof
+			return reverse48(v), eof
 		case 6:
 			v := t.limit56[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit56[t.on8]))
-			return reverse56(v), int(v[6]), eof
+			return reverse56(v), eof
 		default:
 			v := t.limit64[t.on8][t.oncursor]
 			eof := t.forward(len(t.limit64[t.on8]))
-			return reverse64(v), int(v[7]), eof
+			return reverse64(v), eof
 	}
 }
 
